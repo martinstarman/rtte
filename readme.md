9cbe77782bb0fc9e82817c7fcc5e274e8cc08d1f
Martin Starman <martin@goodsailors.com>
2022-02-06 15:56:22 +0100

    Init base SDL2 window with vcpkg

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# RTTE
+# RTEE
 
 RTTE is attempt to create 2D open source game engine for real time tactics games. Games like Commandos Behind Enemy Lines and Beyond the Call of Duty. Every game is set of .yml files that defines base functionality.
 
@@ -11,4 +11,10 @@ RTTE is attempt to create 2D open source game engine for real time tactics games
 
 ## Building & running
 
-TBD
+- clone repo
+- install cmake
+- run ```./vendor/vcpkg/bootstrap-vcpkg.sh```
+- run ```./vendor/vcpkg/vcpkg.exe install```
+- run ```cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=./vendor/vcpkg/scripts/buildsystems/vcpkg.cmake```
+- run ```cmake --build build```
+- run ```./build/debug/rtte.exe```
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("RTTE", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                isRunning = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 105, 180, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
