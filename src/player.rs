14716923d08fa78ad8ce6c464ac21dab3c7606bd
Martin Starman <martin@goodsailors.com>
2024-08-08 21:56:21 +0200

    Player animation speed

--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,27 @@
 use bevy::prelude::*;
+use std::time::Duration;
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct PlayerAnimationConfig {
+  pub fps: u8,
+  pub frame_timer: Timer,
+}
+
+impl PlayerAnimationConfig {
+  fn new(fps: u8) -> Self {
+    Self {
+      fps,
+      frame_timer: Self::timer_from_fps(fps),
+    }
+  }
+
+  fn timer_from_fps(fps: u8) -> Timer {
+    Timer::new(Duration::from_secs_f32(1.0 / (fps as f32)), TimerMode::Once)
+  }
+}
 
 pub fn player_setup(
   mut commands: Commands,
@@ -19,11 +39,21 @@ pub fn player_setup(
       ..default()
     },
     TextureAtlas::from(texture_atlas_handle),
+    PlayerAnimationConfig::new(10),
   ));
 }
 
-pub fn player_animation(mut texture_atlas_q: Query<&mut TextureAtlas, With<Player>>) {
-  for mut texture_atlas in &mut texture_atlas_q {
-    texture_atlas.index = (texture_atlas.index + 1) % 8;
+pub fn player_animation(
+  time: Res<Time>,
+  mut texture_atlas_q: Query<(&mut TextureAtlas, &mut PlayerAnimationConfig), With<Player>>,
+) {
+  for (mut texture_atlas, mut player_animation_config) in &mut texture_atlas_q {
+    player_animation_config.frame_timer.tick(time.delta());
+
+    if player_animation_config.frame_timer.just_finished() {
+      texture_atlas.index = (texture_atlas.index + 1) % 8;
+      player_animation_config.frame_timer =
+        PlayerAnimationConfig::timer_from_fps(player_animation_config.fps);
+    }
   }
 }
