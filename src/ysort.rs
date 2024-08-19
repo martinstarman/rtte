51f61888e18dde69f7ad1b0ca978b04404cf2af7
martinstarman <martinstarman@users.noreply.github.com>
2024-08-19 22:41:32 +0200

    Y Sorting (#82)

---
+++ src/ysort.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct YSort {
+  pub height: u32,
+}
+
+pub fn y_sort(mut query: Query<(&mut Transform, &YSort)>) {
+  for (mut transform, ysort) in &mut query {
+    transform.translation.z = -transform.translation.y + (ysort.height as f32 / 2.);
+  }
+}
