3757fec9de3a2426fc4e449ab392710d34a4e49f
Martin Starman <martin@goodsailors.com>
2024-10-20 21:42:36 +0200

    Move obstacle from navmesh to tree

--- src/navmesh.rs
+++ src/navmesh.rs
@@ -14,16 +14,9 @@ pub fn navmesh_setup(mut commands: Commands) {
       ..default()
     },
     update_mode: NavMeshUpdateMode::Direct,
-    transform: Transform::from_xyz(0., 0., -999.),
+    transform: Transform::default(),
     ..NavMeshBundle::with_default_id()
   });
-
-  // TODO: obstacles should be defined on tree
-  let transform = Transform::from_translation(Vec3::new(-75., 60., 0.0));
-  commands.spawn((
-    PrimitiveObstacle::Rectangle(Rectangle::new(20., 20.)),
-    SpatialBundle::from_transform(transform),
-  ));
 }
 
 pub fn navmesh_draw(
@@ -36,6 +29,7 @@ pub fn navmesh_draw(
   navmesh: Query<(&Handle<NavMesh>, Ref<NavMeshStatus>)>,
 ) {
   let (navmesh_handle, status) = navmesh.single();
+
   if (!status.is_changed() || *status != NavMeshStatus::Built) && window_resized.is_empty() {
     return;
   }
@@ -58,7 +52,7 @@ pub fn navmesh_draw(
       .with_children(|main_mesh| {
         main_mesh.spawn(MaterialMesh2dBundle {
           mesh: meshes.add(navmesh.to_wireframe_mesh()).into(),
-          transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.1)),
+          transform: Transform::from_xyz(0.0, 0.0, 0.1),
           material: materials.add(ColorMaterial::from(Color::srgb(1., 0., 0.))),
           ..default()
         });
@@ -68,11 +62,11 @@ pub fn navmesh_draw(
 }
 
 pub fn navmesh_obstacle_draw(mut gizmos: Gizmos, query: Query<(&PrimitiveObstacle, &Transform)>) {
-  for (prim, transform) in &query {
-    match prim {
-      PrimitiveObstacle::Rectangle(prim) => {
+  for (obstacle, transform) in &query {
+    match obstacle {
+      PrimitiveObstacle::Rectangle(primitive) => {
         gizmos.primitive_2d(
-          prim,
+          primitive,
           transform.translation.xy(),
           transform.rotation.to_axis_angle().1,
           Color::srgb(1., 0., 0.),
--- src/tree.rs
+++ src/tree.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use vleue_navigator::prelude::*;
 
 use crate::ysort::YSort;
 
@@ -7,21 +8,22 @@ pub struct Tree;
 
 pub fn tree_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
   let texture = asset_server.load("tree.png");
+  let x = 100.;
+  let y = 100.;
+  let z = 0.;
 
   commands.spawn((
     Tree,
     SpriteBundle {
       texture,
-      transform: Transform {
-        translation: Vec3 {
-          x: 100.,
-          y: 100.,
-          z: 0.,
-        },
-        ..default()
-      },
+      transform: Transform::from_xyz(x, y, z),
       ..default()
     },
     YSort { height: 116 },
+  ));
+
+  commands.spawn((
+    PrimitiveObstacle::Rectangle(Rectangle::new(10., 10.)),
+    SpatialBundle::from_transform(Transform::from_xyz(x, y - 58., z)),
   ));
 }
