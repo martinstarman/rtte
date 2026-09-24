c3207887a9aa351cb11b432a13af0d9a6f022b1b
martinstarman <martin@goodsailors.com>
2026-09-24 20:26:52 +0200

    feat: sprite dimension and shape

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <vector>
 
 #include "sprite.h"
 
@@ -7,13 +8,27 @@ int main(int argc, char *argv[])
   InitWindow(800, 600, "RTTE");
   SetTargetFPS(60);
 
-  Sprite sprite("sample/sprite.png", Vector2{32.0, 32.0}, Vector2{10.0, 10.0});
+  Sprite sprite1("sample/sprite.png", Vector3{10.0, 10.0, 0.0});
+  Sprite sprite2("sample/sprite.png", Vector3{60.0, 10.0, 0.0}, Vector2{64.0, 64.0});
+
+  std::vector<Vector2> shape = {
+      {0.0, 0.0},
+      {32.0, 0.0},
+      {64.0, 32.0},
+      {32.0, 32.0},
+  };
+
+  Sprite sprite3("sample/sprite.png", Vector3{150.0, 10.0, 0.0}, shape);
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(WHITE);
-    sprite.Render();
+
+    sprite1.Render();
+    sprite2.Render();
+    sprite3.Render();
+
     EndDrawing();
   }
 
--- src/sprite.h
+++ src/sprite.h
@@ -1,19 +1,24 @@
 #pragma once
 
+#include <limits>
 #include <raylib.h>
 #include <string>
+#include <vector>
 
 class Sprite
 {
 public:
-  Sprite(const std::string &path, Vector2 dimensions, Vector2 position);
+  Sprite(const std::string &path, Vector3 position);
+  Sprite(const std::string &path, Vector3 position, Vector2 dimensions);
+  Sprite(const std::string &path, Vector3 position, const std::vector<Vector2> shape);
   ~Sprite();
   void Render();
   const Texture2D &GetTexture() const;
 
 private:
   std::string m_path;
-  Vector2 m_position;
+  Vector3 m_position;
   Vector2 m_dimensions;
+  std::vector<Vector2> m_shape;
   Texture2D m_texture;
 };
